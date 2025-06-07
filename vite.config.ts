f2655d91e6ff215002515cc911a47ae09b9e3a1d
Hitesh Saha <rupam367ro@gmail.com>
2025-06-08 00:55:15 +0530

    lint fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,21 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext',
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          mui: ['@mui/material', '@mui/icons-material'],
-          crypto: ['node-forge'],
-          peer: ['peerjs'],
-        },
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom', '@mui/material', 'peerjs'],
-  },
+  // build: {
+  //   target: 'esnext',
+  //   minify: 'terser',
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         vendor: ['react', 'react-dom', 'react-router-dom'],
+  //         mui: ['@mui/material', '@mui/icons-material'],
+  //         crypto: ['node-forge'],
+  //         peer: ['peerjs'],
+  //       },
+  //     },
+  //   },
+  // },
+  // optimizeDeps: {
+  //   include: ['react', 'react-dom', 'react-router-dom', '@mui/material', 'peerjs'],
+  // },
 });
